3789e55e2deaf4cdb1f103c3e9222b73d868c513
Administrator <Administrator@SD-20181229CKLG>
2019-02-22 11:18:29 +0800

    test

---
+++ src/main/java/com/example/demo/ConsumerController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+	@Autowired
+	private HelloService helloService;
+	
+	@RequestMapping(value = "/feign-consumer",method = RequestMethod.GET)
+	public String helloConsumer() {
+		return helloService.hello();
+	}
+	
+	@RequestMapping(value = "/feign-consumer2",method = RequestMethod.GET)
+	public String helloConsumer2() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(helloService.hello()).append("\n");
+		sb.append(helloService.hello("DIDI")).append("\n");
+		sb.append(helloService.hello("DIDI",30)).append("\n");
+		sb.append(helloService.hello(new User("DIDI",30))).append("\n");
+		return sb.toString();
+	}
+	
+}
---
+++ src/main/java/com/example/demo/HelloService.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "hello-service")
+public interface HelloService {
+
+	@RequestMapping("/hello")
+	String hello();
+	
+	@RequestMapping(value = "/hello1",method = RequestMethod.POST)
+	String hello(@RequestParam("name") String name);
+	
+	@RequestMapping(value = "/hello2",method = RequestMethod.POST)
+	String hello(@RequestParam("name") String name,@RequestHeader("age") Integer age);
+	
+	@RequestMapping(value = "/hello3",method = RequestMethod.POST)
+	String hello(@RequestBody User user);
+	
+	
+	
+}
